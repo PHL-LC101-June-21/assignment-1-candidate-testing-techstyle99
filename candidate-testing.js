c4f0ff1edf98fdf90f2053f46e575ef70d2753a6
techstyle99 <>
2021-06-15 01:20:44 +0000

    this is submitting for homework assignment#1, Part I

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,12 +2,13 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
+
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,18 +16,23 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("Enter your name here: "); 
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  candidateAnswer = input.question(question)
 
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if (candidateAnswers === correctAnswer) {
+    console.log("Your answer is correct!")
+  } else {
+    console.log("Your answer is incorrect!")
+  }
 
 
   let grade;
@@ -38,6 +44,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+    console.log("Hello " + candidateName + ", welcome here.")
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
