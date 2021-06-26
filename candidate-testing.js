f63c6fc1579c68fdd3d72c6f2cb5858191a6a57b
techstyle99 <>
2021-06-26 19:01:25 +0000

    lost original instruction, trying to revert to previous specific commit with git log command

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,65 +1,88 @@
+clear
+
+
+
+
+
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
-
-
-// TODO 1.1a: Define candidateName // 
-let candidateName = "";
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
-
-function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+let candidateName = ""; 
+function askForName(){
   candidateName = input.question("Enter your name here: "); 
+  console.log(`Hello ${candidateName}! welcome.\n`)
+} 
 
-}
+// askForName(); 
 
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+let questions = [
+  "Who was the first American woman in space?: ", 
+  "True or false: 5 kilometer == 5000 meters?: ", 
+  "(5 + 3)/2 * 10 = ?: ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?: ", 
+  "What is the minimum crew size for the ISS?: "
+]; 
 
-}
+let correctAnswers = [
+  "Sally Ride", 
+  "true", 
+  "40", 
+  "Trajectory", 
+  "3"
+]; 
 
-function gradeQuiz(candidateAnswers) {
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswers === correctAnswer) {
-    console.log("Your answer is correct!")
-  } else {
-    console.log("Your answer is incorrect!")
+let answers = []; 
+function askQuestion(){
+  for (i=0; i<questions.length; i++){
+    answer = input.question((`${i+1}) ` + questions[i])); 
+    answers.push(answer); 
+    console.log(`your answer: ${answer}`); 
+    console.log(`correct answer: ${correctAnswers[i]}\n`); 
   }
-
-
-  let grade;
-  
-
-  return grade;
 }
+
+// askQuestion(); 
+
+function gradeQuiz(correctAnswers){
+  let totalQuestion = questions.length
+  let totalCorrectAnswers = 0; 
+  for (i=0; i<questions.length; i++){
+    // how do i access function 'askQuestion'-scoped variable 'answer' in this function ?
+    let answer = answers[i].trim().toLowerCase(); 
+    let correctAnswer = correctAnswers[i].trim().toLowerCase()
+    if (answer === correctAnswer){
+      totalCorrectAnswers += 1; 
+    }
+  }
+      // outside for loop
+      let percentage = (totalCorrectAnswers/totalQuestion)*100; 
+      console.log(`>>> Overall grade: ${percentage}% (${totalCorrectAnswers} of ${totalQuestion} responses correct) <<<`)
+      if (percentage >= 80){
+        console.log(">>> Status: Passed! <<<")
+      } else {
+        console.log(">>> Status: Failed! <<<")
+      }
+}
+
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-    console.log("Hello " + candidateName + ", welcome here.")
-  
+  console.log(`Candidate Name: ${candidateName}`)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
   candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
+  // question: question,
+  // correctAnswer: correctAnswer,
+  // candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
+  // candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
+
